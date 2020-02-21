87aa1e5e10523e50adf1910685cd41077a223319
CarloCGC <carlocgc@hotmail.com>
2020-02-21 15:51:42 +0000

    Json added to shared project content

---
+++ SudokuFu.Shared/Game/Board/TileData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using MonogameTemplate.Core.Logic;
+
+namespace SudokuFu.Shared.Game.Board
+{
+    [Serializable]
+    [DataCreation("TileData")]
+    public class TileData
+    {
+        public Vector2 Size { get; set; }
+
+        public Vector2 Offset { get; set; }
+    }
+}
---
+++ SudokuFu.Shared/Interfaces/ITile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuFu.Shared.Interfaces
+{
+    public interface ITile
+    {
+        void SetNumber(Int32 num);
+    }
+}
